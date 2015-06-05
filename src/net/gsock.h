7f823fb92e2198c22b2347184f78663234070501
gilgil <gilgil@gilgil.net>
2015-06-05 12:53:27 +0900

    Add res varialble for debugging

--- src/net/gsock.h
+++ src/net/gsock.h
@@ -25,55 +25,67 @@ struct GSock {
   operator int() const { return sock_; }
 
   int accept(GSockAddr *sockAddr, socklen_t *addrLen) {
-    return ::accept((int)*this, &sockAddr->addr_, addrLen);
+    int res = ::accept(*this, &sockAddr->addr_, addrLen);
+    return res;
   }
 
   bool bind(GSockAddr* sockAddr, socklen_t addrLen = sizeof(GSockAddr)) {
-    return ::bind((int)*this, &sockAddr->addr_, addrLen) != -1;
+    int res = ::bind(*this, &sockAddr->addr_, addrLen);
+    return  res != -1;
   }
 
   bool close() {
-    return ::close((int)*this) != -1;
+    return ::close(*this) != -1;
   }
 
   bool connect(GSockAddr* sockAddr, socklen_t addrLen = sizeof(GSockAddr)) {
-    return ::connect((int)*this, &sockAddr->addr_, addrLen) != -1;
+    int res = ::connect(*this, &sockAddr->addr_, addrLen);
+    return  res != -1;
   }
 
   bool listen(int backLog) {
-    return ::listen((int)*this, backLog) != -1;
+    int res = ::listen(*this, backLog);
+    return res != -1;
   }
 
   ssize_t recv(void *buf, size_t len, int flags = 0) {
-    return ::recv((int)*this, buf, len, flags);
+    ssize_t res = ::recv(*this, buf, len, flags);
+    return res;
   }
 
   ssize_t recvfrom(void *buf, size_t len, int flags, GSockAddr* srcAddr, socklen_t *addrLen) {
-    return ::recvfrom((int)*this, buf, len, flags, &srcAddr->addr_, addrLen);
+    ssize_t res = ::recvfrom(*this, buf, len, flags, &srcAddr->addr_, addrLen);
+    return res;
   }
 
   ssize_t recvmsg(struct msghdr *msg, int flags) {
-    return ::recvmsg((int)*this, msg, flags);
+    ssize_t res = ::recvmsg(*this, msg, flags);
+    return res;
   }
 
   ssize_t send(const void *buf, size_t len, int flags = 0) {
-    return ::send((int)*this, buf, len, flags);
+    ssize_t res = ::send(*this, buf, len, flags);
+    return res;
   }
 
   ssize_t sendto(const void *buf, size_t len, int flags, GSockAddr *destAddr, socklen_t addrLen) {
-    return ::sendto((int)*this, buf, len, flags, &destAddr->addr_, addrLen);
+    ssize_t res = ::sendto(*this, buf, len, flags, &destAddr->addr_, addrLen);
+    return res;
   }
 
   ssize_t sendmsg(const struct msghdr *msg, int flags) {
-    return ::sendmsg((int)*this, msg, flags);
+    ssize_t res = ::sendmsg(*this, msg, flags);
+    return res;
   }
 
-  bool setsockopt(int level, int optName, const void *optVal = nullptr, socklen_t optLen = 0) {
-    if (optVal == nullptr) {
+  bool setsockopt(int level, int optname, const void *optval = nullptr, socklen_t optLen = 0) {
+    if (optval == nullptr) {
       int temp = 1;
-      return ::setsockopt((int)*this, level, optName, &temp, sizeof(temp)) != -1;
+      int res = ::setsockopt(*this, level, optname, &temp, sizeof(temp));
+      return res != -1;
     }
-    return ::setsockopt((int)*this, level, optName, optVal, optLen) != -1;
+    int res = ::setsockopt(*this, level, optname, optval, optLen) != -1;
+    return res != -1;
   }
 
   bool socket(int domain, int type, int protocol) {
