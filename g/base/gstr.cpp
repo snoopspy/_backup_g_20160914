8855f369f4cfb44ae913e9840758aba443a408c2
gilgil <gilgil@gilgil.net>
2015-06-17 13:36:40 +0900

    Add gtest code

--- g/base/gstr.cpp
+++ g/base/gstr.cpp
@@ -4,3 +4,16 @@ std::ostream& operator << (std::ostream& os, QString s) {
   os << s.toStdString();
   return os;
 }
+
+// ----------------------------------------------------------------------------
+// GTEST
+// ----------------------------------------------------------------------------
+#ifdef GTEST
+#include <gtest/gtest.h>
+
+TEST(GStr, test) {
+  QString qs = "hello world";
+  std::cout << qs << std::endl;
+}
+
+#endif // GTEST
